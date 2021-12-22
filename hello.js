cff21655eb08ea149b9b9554e38ac9fe29674132
aqib-muneeb <mnbalrai@gmail.com>
2021-12-23 00:11:57 +0500

    initialcommit

---
+++ hello.js
@@ -0,0 +1,21 @@
+console.log("running")
+var hello = "hello"
+console.log(hello)
+
+
+function alert1() {
+    alert("Rehma how is your kidny ? have you drink water?")
+}
+
+function input() {
+    var age = prompt("what is your age ")
+    age >= 20 ? console.log("over aged") :
+        console.log("under age")
+
+}
+
+function addOne() {
+    a++
+    document.getElementById("date").innerHTML = a;
+
+}
